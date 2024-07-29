ff0a5c686bba7715f7c1de4ec365d0e043112281
breduarm <bryan.e.armijos@gmail.com>
2024-07-29 13:00:00 -0500

    feat(game): Add TopAppBar and BottomAppBar in the GameScreen

--- app/src/main/java/com/beam/tictactoe/ui/MainActivity.kt
+++ app/src/main/java/com/beam/tictactoe/ui/MainActivity.kt
@@ -4,16 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.beam.tictactoe.ui.screens.board.BoardScreen
@@ -24,27 +40,87 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TicTacToeTheme {
-                Scaffold(modifier = Modifier.padding(horizontal = 24.dp)) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-                        Text(
-                            text = "Tic Tac Toe".uppercase(),
-                            textAlign = TextAlign.Center,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.W700,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 32.dp, bottom = 40.dp),
-                        )
-                        BoardScreen()
-                    }
-                }
+            HomeScreen()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    TicTacToeTheme {
+        Scaffold(
+            topBar = {
+                HomeTopAppBar()
+            },
+            bottomBar = {
+                HomeBottomAppBar()
+            }
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(horizontal = 24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp)
+                )
+                BoardScreen()
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopAppBar() {
+    TopAppBar(
+        title = {
+            Text(text = "Tic Tac Toe", fontSize = 20.sp)
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color(0xFF6200EA),
+            titleContentColor = Color.White,
+            actionIconContentColor = Color.White
+        ),
+        actions = {
+            IconButton(onClick = { /* TODO: Handle settings icon click */ }) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "More")
+            }
+        }
+    )
+}
+
+@Composable
+fun HomeBottomAppBar() {
+    BottomAppBar(
+        containerColor = Color(0xFF6200EA), // Background color similar to the image
+        contentColor = Color.White,
+        tonalElevation = 8.dp,
+        actions = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround,
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Home, contentDescription = "Game")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.AccountBox, contentDescription = "Scoreboard")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Face, contentDescription = "More Games")
+                }
+            }
+        })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
